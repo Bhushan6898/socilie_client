b6f43e16dd84c5d7f13feccc6dd1251f1d8e9404
Mr. Bhushan Gulabrao Patil. <143311716+Bhushan6898@users.noreply.github.com>
2025-08-13 09:16:49 -0700

    add music data

--- src/pages/musiclist.js
+++ src/pages/musiclist.js
@@ -4,9 +4,15 @@ import track3 from '../asset/music/tera-mera-sath-67748.mp3';
 import track4 from '../asset/music/saiyaara-movie-bgm-67652.mp3';
 import track5 from '../asset/music/bas-ek-dhadak-mp3cut-net-67621.mp3';
 import track6 from '../asset/music/mile-ho-tum-humko-1197-67744.mp3';
-import track7 from '../asset/music/ek_raat_vilen.mp3';
-import track8 from '../asset/music/ek_raat_vilen.mp3';
-import track9 from '../asset/music/ek_raat_vilen.mp3';
+import track7 from '../asset/music/cutted-001-1636.mp3';
+import track8 from '../asset/music/ganesharto-16699.mp3';
+import track9 from '../asset/music/har_har_mahadev_sambhu.mp3';
+import track10 from '../asset/music/har_har_sambhu.mp3';
+import track11 from '../asset/music/hasifemaledownloadmingseringtone-16183-67723.mp3';
+import track12 from '../asset/music/meera_ke_prabhu.mp3';
+import track13 from '../asset/music/Prasanna Vadanam Shloka Ringtone Download Mp3 - MobCup.Com.Co.mp3';
+import track14 from '../asset/music/shyam_aan_baso.mp3';
+import track15 from '../asset/music/tpti-21794-67198.mp3';
 
 
 const musicList = [
@@ -15,9 +21,18 @@ const musicList = [
   { name: 'tera-mera-sath', url: track3 },
   { name: 'saiyaara', url: track4 },
   { name: 'bas-ek-dhadak', url: track5 },
-  { name: 'mile-ho-tum-humko', url: track5 },
-  
- 
+  { name: 'mile-ho-tum-humko', url: track6 },
+  { name: 'ek-dantay', url: track7 },
+  { name: 'ganesharto', url: track8 },
+  { name: 'har_har_mahadev_sambhu', url: track9 },
+  { name: 'har_har_sambhu', url: track10 },
+  { name: 'hasifemaledownloadmingse', url: track11 },
+  { name: 'meera_ke_prabhu', url: track12 },
+    { name: 'Prasanna Vadanam Shloka', url: track13 },
+  { name: 'shyam_aan_baso', url: track14 },
+  { name: 'om ganpatay namo', url: track15 },
+
+
 ];
 
 export default musicList;
