cd541df4b363e9c4767b4e57fdb758ee5ea6f9ac
Mr. Bhushan Gulabrao Patil. <143311716+Bhushan6898@users.noreply.github.com>
2025-08-19 01:42:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     const timeout = setTimeout(() => {
       setShowSidebarAndNavbar(isAuthenticated);
       setLoading(false);
-    }, 1000);
+    }, 2000);
 
     return () => clearTimeout(timeout);
   }, [isAuthenticated]);
